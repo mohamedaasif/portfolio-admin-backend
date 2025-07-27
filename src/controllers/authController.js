93198c47bb11c8713c9b9d4cc6649fdfc7437716
mohamedaasif <mohamedaasif.md@gmail.com>
2025-07-27 10:45:57 +0530

    generate jwt token

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,8 @@
+const jwt = require("jsonwebtoken");
 const userService = require("../services/userService");
 const { hashPassword, verifyPassword } = require("../utils/hash");
+
+const secretKey = process.env.SECRET_KEY;
 
 async function signup(req, res) {
   const { name, email, password } = req.body;
@@ -37,14 +40,21 @@ async function signin(req, res) {
     return res.status(400).json({ error: "All fields are required" });
   try {
     const existingUser = await userService.findUserByEmail(email);
-    console.log("existingUser", existingUser);
     if (!existingUser) return res.status(404).json({ error: "No user found" });
     const passwordCheck = await verifyPassword(password, existingUser.password);
     if (!passwordCheck)
       return res.status(404).json({ error: "Incorrect Password" });
+
+    const responseData = {
+      id: existingUser._id.toString(),
+      name: existingUser.name,
+      emailId: existingUser.email,
+    };
+    const token = await jwt.sign(responseData, secretKey, { expiresIn: "1h" });
+
     return res
       .status(200)
-      .json({ message: "Signin successful", email: existingUser.email });
+      .json({ message: "Signin successful", authToken: token });
   } catch {
     console.error("Signin error:", err.message);
     res.status(500).json({ error: "Internal server error" });
